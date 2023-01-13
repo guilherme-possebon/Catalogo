5dc6d4956f57ac3c08c11bdf8cd39585ecdccf7c
Guilherme Possebon <gpossebon67@gmail.com>
2023-01-12 21:55:44 -0300

    Tentando arrumar o bug

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,23 @@ function App() {
   return (
     <div className="App">
       <header>
-        <img src="/Logo Majo.png" alt="Logo da empresa 'Majo' "/>
-        <a href="https://pt-br.facebook.com/majoconfeccoes.com.br" target="blank_" ><img src="/logo-facebook.jpg" alt="" className="facebook" /></a>
-        <a href="https://www.instagram.com/majo_confeccoes/" target="blank_" ><img src="/logo-instagram.jpg" alt="" className="instagram" /></a>
-        <a href="https://wa.me/5199126623" target="blank_"><img src="/whats.png" alt="" className="whats" /></a>
+        <img src="https://drive.google.com/file/d/1abALBz2E9YuF8pWcxJaKJnQnij2wzVeI/view?usp=share_link" alt="Logo da empresa 'Majo' "/>
+        <a href="https://pt-br.facebook.com/majoconfeccoes.com.br" target="blank_" ><img src="https://drive.google.com/file/d/1-fa06BU1P69FW7gmeU9UtnYtfv3Cmi6e/view?usp=share_link" alt="" className="facebook" /></a>
+        <a href="https://www.instagram.com/majo_confeccoes/" target="blank_" ><img src="https://drive.google.com/file/d/1ZN5J6YfdzGTqnhkOa4Lgmzqie6zOFLT0/view?usp=share_link" alt="" className="instagram" /></a>
+        <a href="https://wa.me/5199126623" target="blank_"><img src="https://drive.google.com/file/d/1fOH0-Z8vt7lS4Ofj6RAHO2mwZq94fLLy/view?usp=share_link" alt="" className="whats" /></a>
       </header>
 
-      <Catalogo nome="Blusa Feminina" referencia="959, 959-1" img="/3.jpeg" desc='Blusa térmica feminina, malha pettenati. Disponivel nos tamanhos: P ao G2' det='Mais detalhes' phref="#" />
+      <Catalogo nome="Blusa Feminina" referencia="959, 959-1" img="https://drive.google.com/file/d/1KVricWwHW8iUVzRNoaatoegVhcmNSULy/view?usp=share_link" desc='Blusa térmica feminina, malha pettenati. Disponivel nos tamanhos: P ao G2' det='Mais detalhes' phref="#" />
       <hr />
-      <Catalogo nome="Blusa Feminina" referencia="959, 959-1" img="/4.jpeg" desc='Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi voluptates aliquid aut! Officiis dignissimos, quae optio placeat doloribus nulla. Iusto illum quis quibusdam excepturi perspiciatis laborum labore soluta nihil ab!' det='Mais detalhes' phref="#" />
+      <Catalogo nome="Blusa Feminina" referencia="959, 959-1" img="https://drive.google.com/file/d/15_q7ztmrUs08Go0lhLIIf5MV88StYbQZ/view?usp=share_link" desc='Blusa térmica feminina, malha pettenati. Disponivel nos tamanhos: P ao G2' det='Mais detalhes' phref="#" />
       <hr />
-      <Catalogo nome="Blusa Feminina" referencia="959, 959-1" img="/5.jpeg" desc='Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi voluptates aliquid aut! Officiis dignissimos, quae optio placeat doloribus nulla. Iusto illum quis quibusdam excepturi perspiciatis laborum labore soluta nihil ab!' det='Mais detalhes' phref="#" />
+      <Catalogo nome="Blusa Feminina" referencia="959, 959-1" img="https://drive.google.com/file/d/1F1um7cosnTlKNtlPkRoAQxTPHCttECHm/view?usp=share_link" desc='Blusa térmica feminina, malha pettenati. Disponivel nos tamanhos: P ao G2' det='Mais detalhes' phref="#" />
       <hr />
-      <Catalogo nome="Blusa Feminina" referencia="959, 959-1" img="/6.jpeg" desc='Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi voluptates aliquid aut! Officiis dignissimos, quae optio placeat doloribus nulla. Iusto illum quis quibusdam excepturi perspiciatis laborum labore soluta nihil ab!' det='Mais detalhes' phref="#" />
+      <Catalogo nome="Blusa Feminina" referencia="959, 959-1" img="https://drive.google.com/file/d/1slPOMlcah0b7SiV-yabnLXft72PXj7fP/view?usp=share_link" desc='Blusa térmica feminina, malha pettenati. Disponivel nos tamanhos: P ao G2' det='Mais detalhes' phref="#" />
       <hr />
-      <Catalogo nome="Blusa Feminina" referencia="959, 959-1" img="/7.jpeg" desc='Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi voluptates aliquid aut! Officiis dignissimos, quae optio placeat doloribus nulla. Iusto illum quis quibusdam excepturi perspiciatis laborum labore soluta nihil ab!' det='Mais detalhes' phref="#" />
+      <Catalogo nome="Blusa Feminina" referencia="959, 959-1" img="https://drive.google.com/file/d/14ioDt-STZ0oOWrNhT_dYlg0nnGWHebyW/view?usp=share_link" desc='Blusa térmica feminina, malha pettenati. Disponivel nos tamanhos: P ao G2' det='Mais detalhes' phref="#" />
       <hr />
-      <Catalogo nome="Blusa Feminina" referencia="959, 959-1" img="/8.jpeg" desc='Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi voluptates aliquid aut! Officiis dignissimos, quae optio placeat doloribus nulla. Iusto illum quis quibusdam excepturi perspiciatis laborum labore soluta nihil ab!' det='Mais detalhes' phref="#" />
+      <Catalogo nome="Blusa Feminina" referencia="959, 959-1" img="https://drive.google.com/file/d/1Gw-NSh2skpeo_d_b8N7GeTtVO-M523rB/view?usp=share_link" desc='Blusa térmica feminina, malha pettenati. Disponivel nos tamanhos: P ao G2' det='Mais detalhes' phref="#" />
       <hr />
       
       
