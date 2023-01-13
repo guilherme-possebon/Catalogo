1e499bc4c2731e4d4995183eaae34b08ec7bd992
Guilherme Possebon <gpossebon67@gmail.com>
2023-01-12 21:33:29 -0300

    Tentando Hospedar o site + adicionando

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,24 @@ function App() {
 
   return (
     <div className="App">
-    <hr />
-      <Catalogo nome="macaquinho jeans Tanise" referencia={1239123821} img="/img1.png" desc='Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi voluptates aliquid aut! Officiis dignissimos, quae optio placeat doloribus nulla. Iusto illum quis quibusdam excepturi perspiciatis laborum labore soluta nihil ab!' det='Mais detalhes' phref="#" />
+      <header>
+        <img src="/Logo Majo.png" alt="Logo da empresa 'Majo' "/>
+        <a href="https://pt-br.facebook.com/majoconfeccoes.com.br" target="blank_" ><img src="/logo-facebook.jpg" alt="" className="facebook" /></a>
+        <a href="https://www.instagram.com/majo_confeccoes/" target="blank_" ><img src="/logo-instagram.jpg" alt="" className="instagram" /></a>
+        <a href="https://wa.me/5199126623" target="blank_"><img src="/whats.png" alt="" className="whats" /></a>
+      </header>
+
+      <Catalogo nome="Blusa Feminina" referencia="959, 959-1" img="/3.jpeg" desc='Blusa térmica feminina, malha pettenati. Disponivel nos tamanhos: P ao G2' det='Mais detalhes' phref="#" />
       <hr />
-      <Catalogo nome="macaquinho jeans Tanise" referencia={1239123821} img="/2.jpeg" desc='Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi voluptates aliquid aut! Officiis dignissimos, quae optio placeat doloribus nulla. Iusto illum quis quibusdam excepturi perspiciatis laborum labore soluta nihil ab!' det='Mais detalhes' phref="#" />
+      <Catalogo nome="Blusa Feminina" referencia="959, 959-1" img="/4.jpeg" desc='Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi voluptates aliquid aut! Officiis dignissimos, quae optio placeat doloribus nulla. Iusto illum quis quibusdam excepturi perspiciatis laborum labore soluta nihil ab!' det='Mais detalhes' phref="#" />
       <hr />
-      <Catalogo nome="macaquinho jeans Tanise" referencia={1239123821} img="/3.jpeg" desc='Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi voluptates aliquid aut! Officiis dignissimos, quae optio placeat doloribus nulla. Iusto illum quis quibusdam excepturi perspiciatis laborum labore soluta nihil ab!' det='Mais detalhes' phref="#" />
+      <Catalogo nome="Blusa Feminina" referencia="959, 959-1" img="/5.jpeg" desc='Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi voluptates aliquid aut! Officiis dignissimos, quae optio placeat doloribus nulla. Iusto illum quis quibusdam excepturi perspiciatis laborum labore soluta nihil ab!' det='Mais detalhes' phref="#" />
       <hr />
-      <Catalogo nome="macaquinho jeans Tanise" referencia={1239123821} img="/4.jpeg" desc='Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi voluptates aliquid aut! Officiis dignissimos, quae optio placeat doloribus nulla. Iusto illum quis quibusdam excepturi perspiciatis laborum labore soluta nihil ab!' det='Mais detalhes' phref="#" />
+      <Catalogo nome="Blusa Feminina" referencia="959, 959-1" img="/6.jpeg" desc='Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi voluptates aliquid aut! Officiis dignissimos, quae optio placeat doloribus nulla. Iusto illum quis quibusdam excepturi perspiciatis laborum labore soluta nihil ab!' det='Mais detalhes' phref="#" />
       <hr />
-      <Catalogo nome="macaquinho jeans Tanise" referencia={1239123821} img="/5.jpeg" desc='Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi voluptates aliquid aut! Officiis dignissimos, quae optio placeat doloribus nulla. Iusto illum quis quibusdam excepturi perspiciatis laborum labore soluta nihil ab!' det='Mais detalhes' phref="#" />
+      <Catalogo nome="Blusa Feminina" referencia="959, 959-1" img="/7.jpeg" desc='Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi voluptates aliquid aut! Officiis dignissimos, quae optio placeat doloribus nulla. Iusto illum quis quibusdam excepturi perspiciatis laborum labore soluta nihil ab!' det='Mais detalhes' phref="#" />
       <hr />
-      <Catalogo nome="macaquinho jeans Tanise" referencia={1239123821} img="/6.jpeg" desc='Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi voluptates aliquid aut! Officiis dignissimos, quae optio placeat doloribus nulla. Iusto illum quis quibusdam excepturi perspiciatis laborum labore soluta nihil ab!' det='Mais detalhes' phref="#" />
-      <hr />
-      <Catalogo nome="macaquinho jeans Tanise" referencia={1239123821} img="/7.jpeg" desc='Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi voluptates aliquid aut! Officiis dignissimos, quae optio placeat doloribus nulla. Iusto illum quis quibusdam excepturi perspiciatis laborum labore soluta nihil ab!' det='Mais detalhes' phref="#" />
-      <hr />
-      <Catalogo nome="macaquinho jeans Tanise" referencia={1239123821} img="/8.jpeg" desc='Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi voluptates aliquid aut! Officiis dignissimos, quae optio placeat doloribus nulla. Iusto illum quis quibusdam excepturi perspiciatis laborum labore soluta nihil ab!' det='Mais detalhes' phref="#" />
-      <hr />
-      <Catalogo nome="macaquinho jeans Tanise" referencia={1239123821} img="/img1.png" desc='Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi voluptates aliquid aut! Officiis dignissimos, quae optio placeat doloribus nulla. Iusto illum quis quibusdam excepturi perspiciatis laborum labore soluta nihil ab!' det='Mais detalhes' phref="#" />
-      <hr />
-      <Catalogo nome="macaquinho jeans Tanise" referencia={1239123821} img="/img1.png" desc='Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi voluptates aliquid aut! Officiis dignissimos, quae optio placeat doloribus nulla. Iusto illum quis quibusdam excepturi perspiciatis laborum labore soluta nihil ab!' det='Mais detalhes' phref="#" />
+      <Catalogo nome="Blusa Feminina" referencia="959, 959-1" img="/8.jpeg" desc='Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi voluptates aliquid aut! Officiis dignissimos, quae optio placeat doloribus nulla. Iusto illum quis quibusdam excepturi perspiciatis laborum labore soluta nihil ab!' det='Mais detalhes' phref="#" />
       <hr />
       
       
--- src/components/Catalogo.jsx
+++ src/components/Catalogo.jsx
@@ -4,15 +4,17 @@ import PropTypes from "prop-types"
 
 function Catalogo({nome, referencia, img, desc, phref, det}) {
     return (
-        <div className="div1">
-                <div className="div2">
-                    <h1 className="h1Nome">{nome}</h1>
-                    <p className="pDesc">{desc}</p>
-                    <img src={img} alt=""  className="img"/>
-                    <a href={phref} className="pDet">{det}</a>
-                    <p className="pReferencia">{referencia}</p>
-                </div>
-        </div>
+        <main className="main">
+            <div className="div1">
+                    <div className="div2">
+                        <h1 className="h1Nome">{nome}</h1>
+                        <p className="pDesc">{desc}</p>
+                        <img src={img} alt=""  className="img"/>
+                        <a href={phref} className="pDet">{det}</a>
+                        <p className="pReferencia">{referencia}</p>
+                    </div>
+            </div>
+        </main>
     )
 }
 
