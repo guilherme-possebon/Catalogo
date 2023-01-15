4e574319babae0a23d3afca5b66dc80942a44469
Guilherme Possebon <gpossebon67@gmail.com>
2023-01-15 14:37:20 -0300

    Adicionando mais imagens para o Catalogo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import {Catalogo} from "./components/Catalogo"
+import { Catalogo2 } from "./components/Catalogo"
+import { Catalogo3 } from "./components/Catalogo"
 import {Header} from "./components/Header"
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
       <Header />
 
       <Catalogo nome="Blusa Térmica Feminina" referencia="959, 959-1" desc='Malha pettenati. Disponivel nos tamanhos: P ao G2' />
+      <Catalogo2 nome="Pasta A" referencia="959, 959-1" desc='Descrição da pasta a' />
+      <Catalogo3 nome="Pasta b" referencia="959, 959-1" desc='Descrição da pasta b' />
     </div>
   )
 }
--- src/components/Catalogo.jsx
+++ src/components/Catalogo.jsx
@@ -10,6 +10,26 @@ import n5 from "/5.png"
 import n6 from "/6.png"
 import n7 from "/7.png"
 import n8 from "/8.png"
+import pa1 from "/PastaA1.jpeg"
+import pa2 from "/PastaA2.jpeg"
+import pa3 from "/PastaA3.jpeg"
+import pa4 from "/PastaA4.jpeg"
+import pa5 from "/PastaA5.jpeg"
+import pa6 from "/PastaA6.jpeg"
+import pa7 from "/PastaA7.jpeg"
+import pa8 from "/PastaA8.jpeg"
+import pa9 from "/PastaA9.jpeg"
+import pa10 from "/PastaA10.jpeg"
+import pb1 from "/PastaB1.jpeg"
+import pb2 from "/PastaB2.jpeg"
+import pb3 from "/PastaB3.jpeg"
+import pb4 from "/PastaB4.jpeg"
+import pb5 from "/PastaB5.jpeg"
+import pb6 from "/PastaB6.jpeg"
+import pb7 from "/PastaB7.jpeg"
+
+
+
 import logo from "/Logo_Majo.png"
 
 
@@ -23,93 +43,67 @@ export function Catalogo({nome, referencia, desc,}) {
             <p className="pReferencia">{referencia}</p>
 
             <div className="smallContainer">
-                <CatalagoRow img1={n4} imgn1={n2} img2={n3} img3={n4} det="Mais detalhes"/>
-                <CatalagoRow img1={n5} img2={n6} img3={n7} det="Mais detalhes"/>
-                <CatalagoRow img1={n8} img2={n8} img3={n8} det="Mais detalhes"/>
-
+                <CatalogoRow2 img={n4}/>
+                <CatalogoRow2 img={n3}/>
+                <CatalogoRow2 img={n5}/>
+                <CatalogoRow2 img={n6}/>
+                <CatalogoRow2 img={n7}/>
+                <CatalogoRow2 img={n8}/>
             </div>
         </div>
     )
 }
+export function Catalogo2({nome, referencia, desc,}) {
+    return (
+        <div className="container">
 
-export function CatalagoRow({img1, imgn1, img2, img3, phref, det}) {
-    
-    const [btnState, setBtnState] = useState(false)
-  
-    function handleClick() {
-        setBtnState(btnState => !btnState)
-    }
+            <h1 className="h1Nome">{nome}</h1>
+            <p className="pDesc">{desc}</p>
+            <p className="pReferencia">{referencia}</p>
 
-    let displayNone = btnState ? false : ''
-    
-    let unToggleClassCheck = btnState ? true : false
+            <div className="smallContainer">
+                <CatalogoRow2 img={pa1} />
+                <CatalogoRow2 img={pa2} />
+                <CatalogoRow2 img={pa3} />
+                <CatalogoRow2 img={pa4} />
+                <CatalogoRow2 img={pa5} />
+                <CatalogoRow2 img={pa6} />
+                <CatalogoRow2 img={pa7} />
+                <CatalogoRow2 img={pa8} />
+                <CatalogoRow2 img={pa9} />
+                <CatalogoRow2 img={pa10} />
+            </div>
+        </div>
+    )
+}
+export function Catalogo3({nome, referencia, desc,}) {
+    return (
+        <div className="container">
+
+            <h1 className="h1Nome">{nome}</h1>
+            <p className="pDesc">{desc}</p>
+            <p className="pReferencia">{referencia}</p>
+
+            <div className="smallContainer">
+                <CatalogoRow2 img={pb1} />
+                <CatalogoRow2 img={pb2} />
+                <CatalogoRow2 img={pb3} />
+                <CatalogoRow2 img={pb4} />
+                <CatalogoRow2 img={pb5} />
+                <CatalogoRow2 img={pb6} />
+                <CatalogoRow2 img={pb7} />
+            </div>
+        </div>
+    )
+}
+export function CatalogoRow2({img, img2, img3, img4}) {
 
     return (
         <div className="row">
             <div className="col">
-                <img src={img1} alt=""  className={`img ${displayNone}`}/>
-                <img src={imgn1} alt="" className={`img ${unToggleClassCheck}`} />
-                <p></p>
-                <button className="btn pDet" onClick={handleClick}>Mais detalhes</button>
-                                   
-
-            </div>
-            <div className="col">
-            <img src={img1} alt=""  className={`img ${displayNone}`}/>
-                <img src={imgn1} alt="" className={`img ${unToggleClassCheck}`} />
-                <p></p>
-                <button className="btn pDet" onClick={handleClick}>Mais detalhes</button>     
-
-            </div>
-            <div className="col">
-            <img src={img1} alt=""  className={`img ${displayNone}`}/>
-                <img src={imgn1} alt="" className={`img ${unToggleClassCheck}`} />
-                <p></p>
-                <button className="btn pDet" onClick={handleClick}>Mais detalhes</button>
-
-            </div>
+                <img src={img} alt="" className="img" />
+             </div>
         </div>
     )
 }
 
-
-
-
-
-
-
-
-// export function Detalhe({imgn1, imgn2, imgn3, img1, img2, img3, det}) {
-
-//     const [btnState, setBtnState] = useState(false)
-  
-//     function handleClick() {
-//         setBtnState(btnState => !btnState)
-//     }
-  
-//     let toggleClassCheck = btnState ? ' active' : null
-    
-//     let unToggleClassCheck = btnState ? undefined : ' ativo'
-     
-//     return (
-//         <div className={`btn ${toggleClassCheck}`}>
-
-//             <img src={img1} alt=""  className="img img1"/>
-//             <img src={imgn1} className={`${unToggleClassCheck}`} alt="" />
-//             <button onClick={handleClick}>
-//                 Mais detalhes
-//           </button>
-
-//             <img src={imgn2} className={`${unToggleClassCheck}`} alt="" />
-//             <button onClick={handleClick}>
-//                 Mais detalhes
-//           </button>
-
-//             <img src={imgn3} className={`${unToggleClassCheck}`} alt="" />
-//             <button onClick={handleClick}>
-//                 Mais detalhes
-//           </button>
-
-//         </div>
-//     )
-//   }
